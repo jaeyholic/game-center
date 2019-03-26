7035e54586034f782e8ad842c5bd9cf3805aa114
Felix Nana Yaw Yeboah-Jefferson <gabsco208309@hotmail.com>
2019-03-26 10:51:48 +0000

    conditionals and default props

--- src/components/GamesList.jsx
+++ src/components/GamesList.jsx
@@ -6,14 +6,22 @@ import GameCard from "./GameCard";
 
 const GamesList = ({ games }) => (
   <div className="ui four cards">
-    {games.map(game => (
-      <GameCard game={game} key={game._id} />
-    ))}
+    {games.length === 0 ? (
+      <div className="ui icon message">
+        <i className="icon info" />
+        <div className="content">
+          <div className="header">There are no games in your store</div>
+          <p>You should add some</p>
+        </div>
+      </div>
+    ) : (
+      games.map(game => <GameCard game={game} key={game._id} />)
+    )}
   </div>
 );
 
 GamesList.propTypes = {
-  games: PropTypes.arrayOf(PropTypes.onject).isRequired
+  games: PropTypes.arrayOf(PropTypes.object).isRequired
 };
 
 export default GamesList;
